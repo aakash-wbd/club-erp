245535fd54bd661261c2f9d6110d0f61a4dfa9e8
aakash.wbd <aakash.wbd@gmail.com>
2024-11-15 10:36:00 +0600

    initial commit

---
+++ src/app/providers.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  FluentProvider,
+  RendererProvider,
+  SSRProvider,
+  createDOMRenderer,
+  renderToStyleElements,
+  teamsLightTheme,
+} from "@fluentui/react-components";
+import { useServerInsertedHTML } from "next/navigation";
+import * as React from "react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [renderer] = React.useState(() => createDOMRenderer());
+  const didRenderRef = React.useRef(false);
+
+  useServerInsertedHTML(() => {
+    if (didRenderRef.current) {
+      return;
+    }
+    didRenderRef.current = true;
+    return <>{renderToStyleElements(renderer)}</>;
+  });
+
+  return (
+    <RendererProvider renderer={renderer}>
+      <SSRProvider>
+        <FluentProvider theme={teamsLightTheme}>{children}</FluentProvider>
+      </SSRProvider>
+    </RendererProvider>
+  );
+}
