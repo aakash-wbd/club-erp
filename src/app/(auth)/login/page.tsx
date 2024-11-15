fc1bda7502092018f6a0a46c44ee2622b2e0c81c
aakash.wbd <aakash.wbd@gmail.com>
2024-11-15 16:24:30 +0600

    login form component added

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import CheckBox from "@/components/ui/CheckBox";
-import TextBox from "@/components/ui/TextBox";
-import { Button, Typography } from "antd";
+import Form from "./Form";
 
 const Login = () => {
-  return (
-    <form className="flex flex-col gap-2">
-      <Typography.Text className="text-3xl font-semibold">
-        Welcome Back
-      </Typography.Text>
-      <Typography.Text className="text-sm">
-        Enter your email to receive a one-time password
-      </Typography.Text>
-      <TextBox label="Email" placeholder="Enter your email" />
-      <CheckBox label="Remember Me" />
-      <Button type="primary" className="mt-2">
-        Send 4-digit code
-      </Button>
-    </form>
-  );
+  return <Form />;
 };
 
 export default Login;
